a42113d4854408ce8b7f629c12d54729eaaea0c1
MrMasterplan <simon@heisterkamp.dk>
2011-12-15 11:45:30 +0100

    upps1 updates $STY

--- bash_functions/update_prompt.sh
+++ bash_functions/update_prompt.sh
@@ -1,22 +1,26 @@
 
-function update_prompt {
-    if [ -z "$STY" ]; then
-        PS1="\$(date +%H:%M) \h \W> "
-        return 0
-    fi
+# function update_prompt {
+#     if [ -z "$STY" ]; then
+#         PS1="\$(date +%H:%M) \h \W> "
+#         return 0
+#     fi
+# 
+#     local session_base=$(echo $STY | sed 's/[0-9]*\.\(.*\)/\1/g')
+#     local sessions="$(screen -ls $session_base| grep "$session_base")"
+#     local n_sessions="$(echo "$sessions" | wc -l)"
+#     
+#     if [ "$n_sessions" -lt 2 ]; then
+#         PS1="[$session_base] \$(date +%H:%M) \h \W> "
+#         return 0
+#     fi
+#     
+#     local i_session="$(echo "$sessions" | grep -n $STY | sed 's/^\([0-9]*\):.*/\1/g')"
+#     
+#     PS1="[$session_base:$i_session/$n_sessions] \$(date +%H:%M) \h \W> "
+# }
 
-    local session_base=$(echo $STY | sed 's/[0-9]*\.\(.*\)/\1/g')
-    local sessions="$(screen -ls $session_base| grep "$session_base")"
-    local n_sessions="$(echo "$sessions" | wc -l)"
-    
-    if [ "$n_sessions" -lt 2 ]; then
-        PS1="[$session_base] \$(date +%H:%M) \h \W> "
-        return 0
-    fi
-    
-    local i_session="$(echo "$sessions" | grep -n $STY | sed 's/^\([0-9]*\):.*/\1/g')"
-    
-    PS1="[$session_base:$i_session/$n_sessions] \$(date +%H:%M) \h \W> "
+function update_prompt {
+    export STY="$(screen -ls | grep --color=no -o "$PPID[^[:space:]]*")"
 }
 
 alias upps1="update_prompt"
