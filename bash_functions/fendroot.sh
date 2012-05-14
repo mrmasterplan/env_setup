46dad75ad3a5f420089e1a2010d9b84cd35e5612
MrMasterplan <simon@heisterkamp.dk>
2012-05-14 09:18:04 +0200

    Function to remotely open a root session on fend02

--- bash_functions/fendroot.sh
+++ bash_functions/fendroot.sh
@@ -1,6 +1,7 @@
 
 if [ x"$LOCALNAME" == x"SIMON_MBP" ]; then
     function froot {
+        echo "Now running root on fend02..."
         ssh -X -Y -t fend02 "source ~/.bashrc; module load hep root; root -l $@"
     }
 fi
