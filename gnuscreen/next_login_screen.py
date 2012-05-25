24ce0f829ef4c32fdef8951c48fee79a72a3bc95
MrMasterplan <simon@heisterkamp.dk>
2012-05-25 08:57:52 +0200

    now using a placeholder to stop other scripts from taking the session

--- gnuscreen/next_login_screen.py
+++ gnuscreen/next_login_screen.py
@@ -1,108 +1,65 @@
 #!/usr/bin/env python
 
+# the base name of screen sessions will be:
+base_name="login_"
 
-def main():
-    import os, sys, re, commands, time
-    t0 = time.time()
-    
-    os.system("screen -ls")
-    
-    waitforsimilar()
-    while time.time()-t0<2:
-            time.sleep(0.1)
-    
-    screenls = commands.getoutput("screen -ls")
-    
-    screenrc=""
-    if len(sys.argv) > 1:
-        screenrc= "-c "+os.path.expanduser(os.path.expandvars(sys.argv[1]))
-    
-    base_name="login_"
-    
-    # find attached or detached sceen sessions:
-    query="""\t(?P<full>[0-9]+\."""+base_name+"""(?P<index>[0-9]+))\t\(%s\)"""
-    detached=[int(match.group("index")) for match in re.finditer(query%"Detached",screenls)]
-    attached=[int(match.group("index")) for match in re.finditer(query%"Attached",screenls)]
-    
-    print "           connecting to",
-    if detached:
-        # use the lowest free index
-        print "DETACHED session named",
-        index = min(detached)
+import os, sys, re, commands, time
+
+screenls = commands.getoutput("screen -ls")
+
+# find the screen command line arguments
+screenrc=""
+if len(sys.argv) > 1:
+    screenrc= "-c "+os.path.expanduser(os.path.expandvars(sys.argv[1]))
+
+# find attached or detached sceen session indices:
+query=re.compile(r"\t(?P<full>[0-9]+\."+base_name+r"(?P<index>[0-9]+))\t\((?P<state>.*?)\)")
+detached=[]
+attached=[]
+for match in query.finditer(screenls):
+    if match.group("state") == "Detached":
+        detached+=[int(match.group("index"))]
     else:
-        print "NEW session named",
-        if attached:
-            # or extend the list by one
-            index = max(attached) +1
-        else:
-            # or start a new list
-            print "NEW session named",
-            index=0
-    
-    print "%s%d"%(base_name,index)
-    
-    command="screen %s -RR %s%d"%(screenrc,base_name,index)
-    # pass control to screen:
-    os.execvp("screen",command.split())
+        attached+=[int(match.group("index"))]
 
+# find out which screen session you will attach to:
+if detached:
+    # use the lowest free index
+    index = min(detached)
+else:
+    if attached:
+        # or extend the list by one
+        index = max(attached) +1
+    else:
+        # or start a new list
+        index=0
 
-def waitforsimilar():
-    import os, commands, re, time, random
-    mypid = os.getpid()
-    
-    pspat = re.compile(r""" *(?P<pid>\d+) +(((?P<days>\d+)-)?(?P<hours>\d{2}):)?(?P<time>\d{2}:\d{2}) (?P<command>.*)""")
-    mypspat=re.compile(r""" *%d +(((?P<days>\d+)-)?(?P<hours>\d{2}):)?(?P<time>\d{2}:\d{2}) (?P<command>.*)"""%(mypid))
-    psout=commands.getoutput("ps xo pid,etime,command")
-    match=mypspat.search(psout)
-    mycommand = match.group("command")
-    # print "mycommand:", mycommand
-    mytime = makefloattime(match)
-    
-    lastequals=[]
-    step=0.2
-    while 1:
-        bbs=[]
-        equals=[]
-        psout = commands.getoutput("ps axo pid,etime,command")
-        # print psout
-        mytime = makefloattime(mypspat.search(psout))
-        for match in pspat.finditer(psout):
-            if match.group("command") == mycommand:
-                pid=int(match.group("pid"))
-                if pid==mypid:
-                    continue
-                if makefloattime(match) > mytime:
-                    # print "Found bb:"
-                    # print match.group(0)
-                    bbs=1
-                    break
-                if makefloattime(match) == mytime:
-                    # print "Found equal:"
-                    # print match.group(0)
-                    
-                    equals+=[pid]
-        # found the list of big brothers.
-        if bbs:
-            time.sleep(step)
-            continue
-        if equals:
-            if set(equals) != set(lastequals):
-                lastequals=equals
-                time.sleep(step)
-                continue
-            else:
-                #we are waiting for the same equals as last time.
-                # get impatient.
-                if random.random()<0.1:
-                    return
-        return
+session = "%s%d"%(base_name,index)
 
-def makefloattime(match):
-    time = 60. * int(match.group("time")[:2]) + int(match.group("time")[-2:])
-    if match.group("days"):
-        time+=3600.*24*int(match.group("days"))
-    if match.group("hours"):
-        time+=3600. * int(match.group("hours"))
-    return time
+# get screen socket folder:
+match=re.search(r"[0-9]+ Sockets? in (?P<dir>/.*)\.",screenls)
+screen_dir = match.group("dir")
 
-main()
+# reserve the screen session by creating a named pipe with its name:
+placeholder =screen_dir+"/0000."+session
+os.mkfifo(placeholder)
+
+# tell the user what you are doing:
+print screenls
+print "           connecting to",
+if detached:
+    print "DETACHED",
+else:
+    print "NEW",
+
+print "session named",session
+
+# and wait for her to read it:
+time.sleep(2)
+
+# remove the placeholder 
+os.unlink(placeholder)
+
+command="screen %s -RR %s"%(screenrc,session)
+# pass control to screen:
+os.execvp("screen",command.split())
